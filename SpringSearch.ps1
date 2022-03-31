6f44d1a666e866a15ce2416a474012ab5e49c03b
xstag0 <54673576+securethelogs@users.noreply.github.com>
2022-03-31 11:45:54 +0100

    Create SpringSearch.ps1
    Work in progress ****

---
+++ SpringSearch.ps1
@@ -0,0 +1,42 @@
+
+   <# 
+   Wrote to detect SpringFramework Usage: (Work in progress)
+   @XStag0
+   
+   #>
+
+   $loc = "Set your file path ***"
+
+   Write-Host "[*] Checking For JAVA: "
+   try { java.exe -version } catch { Write-Host "Java may not be installed" }
+   Write-Output ""
+
+   $j = (Get-ChildItem "C:\Program Files\Java\" -Include "*jdk*" -ErrorAction SilentlyContinue).Name
+
+   if ($j){ Write-Host "[*]JDK Found: $j"} else { Write-Host "[*] JDK not found in default path"}
+
+    Write-Output ""
+
+    Write-Host "[*] Searching Files For .JAR and Mention of SpringFramework"
+    Write-Output ""
+
+   foreach ($x in (Get-ChildItem $loc -Include "*.xml", "*.js", "*.jsp", "*.jar", "*.class"  -Recurse -ErrorAction SilentlyContinue).FullName){
+
+    if ($x.EndsWith(".jar")){ 
+        
+        Write-Host "[*] Jar File Found: $x"
+    
+    } else {
+
+    if (Select-String -Path $x -Pattern "springframework"){
+
+        Write-Host "[!] SpringFramework Mentioned Here: $x"
+        
+    }
+
+    }
+
+   }
+
+   Write-Output ""
+
